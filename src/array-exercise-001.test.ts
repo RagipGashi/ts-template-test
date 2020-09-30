c2df2c482b48e04a5372ba067ea16fc8eba22123
KlugeCh <christiankluge9@gmail.com>
2020-09-30 11:27:52 +0200

    added 2nd iteration, final solution

--- src/array-exercise-001.test.ts
+++ src/array-exercise-001.test.ts
@@ -10,18 +10,17 @@ describe('array tests', () => {
     const oddArr = getOddNumbers(input);
     expect(oddArr).toEqual(output);
   });
-  /*
+
   it('test iteration 2', () => {
     // test
     const input = [0, 1, 2, 3, 2];
-    const output = getOddNumbers(input);
+    const output = [1, 3];
 
     // assert
-    const expectedResult = [0, 2, 2];
-    expect(output).toEqual(expectedResult);
+    const oddArr = getOddNumbers(input);
+    expect(oddArr).toEqual(output);
   });
 
   // feel free to create more test iterations for different use cases.
   // finding normal uses cases are easy, but there might be some edge cases ;).
-  */
 });
